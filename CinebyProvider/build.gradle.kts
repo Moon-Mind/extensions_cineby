5e3325683d8ce687e3ffef753ba6ad656b43267a
moonmind <cs.oi.ne.rr.as.dch@tutamail.de>
2026-04-25 22:31:40 +0200

    fix selection

---
+++ CinebyProvider/build.gradle.kts
@@ -0,0 +1,19 @@
+// Use an integer for version numbers
+version = 1
+
+android {
+    namespace = "recloudstream.cineby"
+}
+
+cloudstream {
+    description = "Watch movies and TV series from Cineby"
+    authors = listOf("User")
+    language = "en"
+
+    status = 1
+
+    tvTypes = listOf("Movie", "TvSeries")
+    iconUrl = "https://www.cineby.sc/favicon.ico"
+
+    isCrossPlatform = false
+}
